568c918d4d4f0030a4b7b3442f2228bca0105790
brendan-chess <brendanchess25@gmail.com>
2023-06-14 11:51:44 -0500

    profile login redirect
    redirects unauthed users to sign in and they return to the profile page once they are authed

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,18 +5,18 @@ import { redirect } from "next/navigation";
 export default async function Profile() {
   const session = await getServerSession(authOptions);
   if (!session) {
-    redirect("/api/auth/signin");
+    redirect("/api/auth/signin?callbackUrl=/profile");
   }
   if (session?.user?.email === process.env.ADMIN_EMAIL) {
     return (
       <div className="z-10 w-full max-w-5xl items-center justify-around font-mono text-sm lg:text-xl flex text-black dark:text-white">
         Welcome Administrator
       </div>
-    )
+    );
   }
   return (
-      <div className="z-10 w-full max-w-5xl items-center justify-around font-mono text-sm lg:text-xl flex text-black dark:text-white">
-        This is the profile page! 
-      </div>
-  )
+    <div className="z-10 w-full max-w-5xl items-center justify-around font-mono text-sm lg:text-xl flex text-black dark:text-white">
+      This is the profile page!
+    </div>
+  );
 }
