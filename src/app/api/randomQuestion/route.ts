c8de755f7776da7fef3bd785a0b0cfe1cece3c61
Dominick Agnello <dagnello@asu.edu>
2023-07-26 00:26:35 -0700

    added hacked together option generation that creates card options based on the type and color of the randomly generated card

--- src/app/api/randomQuestion/route.ts
+++ src/app/api/randomQuestion/route.ts
@@ -3,14 +3,37 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
 
+  let query_string: string = "?q="
+
   let card_name: string;
   let card_image: string;
 
   const res_random = await fetch("https://api.scryfall.com/cards/random", { cache: 'no-store' })
+  if (res_random.status == 429) {
+    return NextResponse.error()
+  }
+
   const card = await res_random.json()
 
-  console.log(card);
+  //console.log(card);
 
+  let card_typeline: string = card.type_line;
+
+
+  query_string = query_string + `type:${card_typeline.split(" ")[0].toLowerCase()}`
+
+  if (card.color_identity) {
+    let color_string: string = "color:";
+    card.color_identity.forEach((color: string) => {
+      color_string = color_string + color;
+    });
+    query_string = query_string + "+" + color_string;
+  }
+  else {
+    query_string = query_string + `+cmc:${card.cmc}`;
+  }
+
+  console.log(query_string);
 
   //const res_similar = await fetch(`https://api.scryfall.com/cards/search?q=${"tst"}`);
   //grab the color(s), and the type line
@@ -18,14 +41,22 @@ export async function GET(request: Request) {
   //attach it to the api call
   //pick three random cards
   //create those options
+  //
+  const res_options = await fetch(`https://api.scryfall.com/cards/search${encodeURI(query_string)}`, { cache: 'no-store' })
+
+  const test_options = await res_options.json()
+  console.log(test_options.total_cards);
+
+  const card_options = test_options.data;
+
 
 
   card_name = card.name;
   card_image = card.image_uris.png;
 
-  console.log(card_image);
+  //console.log(card_image);
 
-  const options = [card.name, "Option 2", "Option 3", "Option 4"]
+  const options = [card.name, card_options[0].name, card_options[1].name, card_options[2].name];
   const shuffled_options = shuffle(options);
   const answer = shuffled_options.indexOf(card.name);
   const artURL = card_image;
